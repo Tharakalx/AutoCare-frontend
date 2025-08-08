5afc9a953decba602824686b4eb11220b51adcb1
chamya munasinghe <chamyamunasinghe5@gmail.com>
2025-08-08 16:52:27 +0530

    add other files

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
-import { useState } from 'react'
 
 
 import './App.css'
-import SignIn from './pages/SignIn'
 
+import OurServices from './pages/OurServices'
+import Appointments from './pages/Appointments'
+import AdminDashboard  from './pages/AdminDashboard'
 
 function App() {
   
@@ -12,13 +13,13 @@ function App() {
     <>
 
     
-    <SignIn />
-    <SignIn />
+    <OurServices/>
+    <Appointments/>
+    <AdminDashboard/>
+    
   
-   
-     
     </>
-  )
+  )   
 }
 
 export default App
