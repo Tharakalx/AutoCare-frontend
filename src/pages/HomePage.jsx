509ca5d751de91793c615afa6ad75425bc3e0c43
tharakalx <tharakal352@gmail.com>
2025-08-08 20:08:54 +0530

    create register form and add navbar and fooer components to reagister page

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const HomePage = () => {
+  return (
+    <div>
+      <h1>Welcome to AutoCare</h1>
+      <p>Your one-stop solution for all automotive needs.</p>
+    </div>
+  )
+}
+
+export default HomePage
